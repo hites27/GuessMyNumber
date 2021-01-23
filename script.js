780b5d5853193407d4401a7b22bd6c4662bb509b
Hites Chitalia <hites27@gmail.com>
2021-01-23 11:28:13 +0530

    Refactored code

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number!🎉🎉';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
